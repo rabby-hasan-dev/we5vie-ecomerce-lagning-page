cf4bf0a6502aaef9e9ded6dfa75b4403c237a456
rabby <rabby.65301@gmail.com>
2024-11-24 17:29:49 +0600

    added product details overview card

---
+++ src/app/(WithComonLayout)/product/_components/Module/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,46 @@
+
+'use client'
+import { useEffect, useState } from "react";
+import Container from "@/Components/UI/Container";
+import Slider from "@/Components/Slider/Slider";
+import SlideContent from "@/Components/Slider/SlideContent";
+
+export interface ISlide {
+    image: string;
+    title: string;
+    description: string;
+    buttonText: string;
+}
+
+const ProductSlider = () => {
+    const [slidesData, setSlidesData] = useState([]);
+
+    // Fetch data on component mount
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("/sliderData.json");
+            const data = await response.json();
+            setSlidesData(data);
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <Container>
+            <Slider
+                slides={slidesData.map((slide: ISlide, index) => (
+                    <SlideContent
+                        key={index}
+                        imageSrc={slide.image}
+                        title={slide.title}
+                        description={slide.description}
+                        buttonText={slide.buttonText}
+                    />
+                ))}
+            />
+        </Container>
+    );
+};
+
+export default ProductSlider;
