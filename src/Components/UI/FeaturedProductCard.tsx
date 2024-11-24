6330a75027ed9f674b579177b8545658a33313c9
rabby <rabby.65301@gmail.com>
2024-11-24 15:20:50 +0600

    added Fetured Product section

---
+++ src/Components/UI/FeaturedProductCard.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+
+
+const FeaturedProductCard = () => {
+    return (
+        <div className="shadow-md hover:shadow-lg transition-shadow w-full max-w-sm overflow-hidden mx-auto m-2  border border-gray-200">
+            {/* Image Section */}
+            <div className="relative ">
+                <Image
+                    src="/hero.png"
+                    alt="Product-image"
+                    layout="responsive"
+                    width={1920}
+                    height={1080}
+                    className="object-cover m-2"
+                />
+            </div>
+
+            {/* Content Section */}
+            <div className="p-2">
+
+                {/* Product Info */}
+                <div className="flex justify-between items-center mb-3">
+                    <h3 className="text-lg">Indian Sharee</h3>
+                    <p className="text-xl font-bold text-black">BDT 2,300</p>
+                </div>
+
+                {/* Add to Cart Button */}
+                <button
+                    type="button"
+                    className="w-full mt-4 py-2 border hover:bg-purple-600 hover:text-white text-purple-600 border-purple-600  font-semibold rounded-lg"
+                >
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+
+
+
+    );
+};
+
+export default FeaturedProductCard;
--- src/app/(WithComonLayout)/_components/module/FeaturedProject/FeaturedProject.tsx
+++ src/app/(WithComonLayout)/_components/module/FeaturedProject/FeaturedProject.tsx
@@ -1,10 +1,43 @@
+import { ArrowLeftIcon, ArrowRightIcon } from "@/assets/icons";
 import Container from "@/Components/UI/Container";
+import FeaturedProductCard from "@/Components/UI/FeaturedProductCard";
 import HeadingComponent from "@/Components/UI/HeadingComponent";
 
 const FeaturedProject = () => {
     return (
         <Container>
-            <HeadingComponent heading="New Arrivals" subHeading="featured product" />
+            {/* header part */}
+            <div className="flex justify-between items-center">
+                <HeadingComponent heading="New Arrivals" subHeading="featured product" />
+                <div className="space-x-2">
+                    <button
+                        className="transform -translate-y-1/2 border border-purple-600 hover:bg-purple-600 text-white p-2 rounded-full"
+
+                    >
+                        <ArrowLeftIcon className="text-purple-600 hover:text-white" />
+                    </button>
+                    <button
+                        className=" transform -translate-y-1/2 border border-purple-600 hover:bg-purple-600 text-white p-2 rounded-full"
+
+                    >
+                        <ArrowRightIcon className="text-purple-600 hover:text-white" />
+                    </button>
+                </div>
+            </div>
+
+            {/* main content */}
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 mt-6">
+                <FeaturedProductCard />
+                <FeaturedProductCard />
+                <FeaturedProductCard />
+                <FeaturedProductCard />
+
+            </div>
+
+            <div className="text-center mt-6">
+                <button className="font-semibold px-6 py-2 bg-purple-600 rounded-lg text-white" >See more</button>
+            </div>
+
         </Container>
     );
 };
